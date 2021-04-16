51b857a768b71ca38a4d9e2cec8fd2f20de2fa07
Jose Manuel <jmdcarq89@gmail.com>
2021-04-16 23:08:55 +0200

    material

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { Canvas } from "react-three-fiber";
+import Pinacle from "./components/Pinacle/Pinacle";
+import { OrbitControls } from "@react-three/drei";
 
 function App() {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <h1>hola mundo</h1>
+      <div styled={{ width: "100vw", height: "100vh" }}>
+        <Canvas camera={{ zoom: 2000, position: [15, 500, 500] }}>
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={null}>
+            <Pinacle />
+          </Suspense>
+          <OrbitControls />
+        </Canvas>
+      </div>
     </>
   );
 }
--- src/components/Pinacle/Pinacle.js
+++ src/components/Pinacle/Pinacle.js
@@ -1,15 +1,19 @@
+import * as THREE from "three";
 import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
-export default function Model(props) {
+export default function Pinacle(props) {
+  const material = new THREE.MeshStandardMaterial({
+    color: 0xff0000,
+    transparent: false,
+    opacity: 1,
+  });
+
   const group = useRef();
-  const { nodes, materials } = useGLTF("/torrecompress.glb");
+  const { nodes } = useGLTF("/pinacle.glb");
   return (
     <group ref={group} {...props} dispose={null}>
-      <mesh
-        geometry={nodes.Modelotorresimpl.geometry}
-        material={materials["default"]}
-      />
+      <mesh geometry={nodes.Modelotorresimpl.geometry} material={material} />
     </group>
   );
 }
